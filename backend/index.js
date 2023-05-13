0a62f223d53cbe6dffc41890643192d076a0c612
RimRaider639 <madhurimapoddar639@gmail.com>
2023-05-13 14:45:30 +0530

    connected index.js with routes

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const connection = require("./config/db");
+const usersRouter = require("./routes/users.route");
+const eventsRouter = require("./routes/events.route");
+const requestsRouter = require("./routes/requests.route");
 require("dotenv").config();
 
 const app = express();
+app.use(express.json(), cors());
+app.use("/users", usersRouter);
+app.use("/events", eventsRouter);
+app.use("/requests", requestsRouter);
 
 app.listen(process.env.PORT, async () => {
   console.log(`Server started at port ${process.env.PORT}`);
